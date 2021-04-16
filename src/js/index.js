2b842b0f1503ac9af9a613435edef6fb78743720
onurtaskirancom <taskiranonur9@gmail.com>
2021-04-17 00:41:47 +0300

    Movie data fetched

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,14 @@
-// import {sum,mult} from './lib';
-// import * as lib from './lib';
-import myLib from './lib';
-const lib = new myLib();
+// api : a6730432ddfd8926e4cf3d396ae5b3c0
+// url : https://api.themoviedb.org/3
+// https://api.themoviedb.org/3/search/movie?api_key=a6730432ddfd8926e4cf3d396ae5b3c0&language=en-US&query=abc&page=1&include_adult=false
 
-console.log(lib.sum(1,2,3));
-console.log(lib.mult(1,2,3,4));
+async function getResults(keyword) {
+    const api_key = "a6730432ddfd8926e4cf3d396ae5b3c0";
+    const base_url = "https://api.themoviedb.org/3"
+
+    const response = await fetch(`${base_url}/search/movie?api_key=${api_key}&page=1&query=${keyword}`);
+    const data = await response.json();
+    console.log(data.results);
+}
+
+getResults('abc');
